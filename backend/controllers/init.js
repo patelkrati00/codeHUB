0fb2c2e40703e361f4ac9aa2306eb555609a4290
patelkrati00 <patelkrati00@gmail.com>
2025-09-17 22:46:56 +0530

    init command

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -1,5 +1,23 @@
-const initRepo = ()=>{
-    console.log("initialised a repo");
+import { promises as fs } from "fs";
+import path from "path";
+
+const initRepo = async () => {
+    const repoPath = path.resolve(process.cwd(), ".apnaGit");
+    const commitsPath = path.join(repoPath, "commits");
+
+    try {
+        await fs.mkdir(repoPath, { recursive: true });
+        await fs.mkdir(commitsPath, { recursive: true });
+        await fs.writeFile(
+            path.join(repoPath, "config.json"),
+            JSON.stringify({bucket: process.env.S3_BUCKET})
+        )
+        console.log("repository initialised");
+
+    } catch (error) {
+        console.log("error initialising repo", error);
+    }
+
 }
 
 export default initRepo;
