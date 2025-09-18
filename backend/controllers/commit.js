d034b4e8d412b6c216d2c9c6001ea19c7008a6be
patelkrati00 <patelkrati00@gmail.com>
2025-09-18 16:58:36 +0530

    commit command

--- backend/controllers/commit.js
+++ backend/controllers/commit.js
@@ -1,5 +1,28 @@
-const commitRepo = ()=>{
-    console.log("commited file");
+import { promises as fs } from "fs";
+import path from "path";
+import { v4 as uuidv4 } from "uuid";
+
+const commitRepo = async (message) => {
+    const repoPath = path.resolve(process.cwd(), ".apnaGit");
+    const stagedPath = path.join(repoPath, "staging");
+    const commitPath = path.join(repoPath, "commits")
+
+    try {
+        const commitId = uuidv4();
+        const commitDir = path.join(commitPath, commitId);
+        await fs.mkdir(commitDir, { recursive: true })
+
+        const files = await fs.readdir(stagedPath);
+        for (const file of files) {
+            await fs.copyFile(path.join(stagedPath, file), path.join(commitDir, file))
+        }
+
+        await fs.writeFile(path.join(commitDir, "commit.json"), JSON.stringify({ message, date: new Date().toISOString() }))
+
+        console.log(`committed with id ${commitId} with msg ${message}`)
+    } catch (error) {
+        console.log("error in commiting files", error);
+    }
 }
 
 export default commitRepo;
