bf765a31e739f4ac594e400c89f66de7895c44f4
patelkrati00 <patelkrati00@gmail.com>
2025-09-29 17:48:37 +0530

    login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -45,7 +45,7 @@ export const signup = async (req, res) => {
         }
 
         const result = await userCollection.insertOne(newUser);
-        const token = jwt.sign({ id: result.insertId }, process.env.JWT_SECRET_KEY, { expiresIn: "1h" })
+        const token = jwt.sign({ id: result.insertedId }, process.env.JWT_SECRET_KEY, { expiresIn: "1h" })
         res.send({ token });
     } catch (error) {
         console.log("err during signup", error);
@@ -53,8 +53,30 @@ export const signup = async (req, res) => {
     }
 };
 
-export const login = (req, res) => {
-    res.send("login");
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        await connectionClient();
+        const db = client.db("githubclone")
+        const userCollection = db.collection("users");
+
+        const user = await userCollection.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({ message: "Invalid credentials" })
+        }
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: "1h" })
+        res.send({ token, userId: user._id });
+
+    } catch (err) {
+        console.log("Error in login", err)
+        return res.status(500).json({ message: "server error" })
+    }
 };
 
 export const getUserProfile = (req, res) => {
