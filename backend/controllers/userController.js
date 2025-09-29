7f6747f4ff9e98cd26bbe805b0d09908b21f5416
patelkrati00 <patelkrati00@gmail.com>
2025-09-29 16:33:05 +0530

    user signup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,56 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv"
+
+dotenv.config();
+const uri = process.env.MONGO_URL;
+
+let client;
+
+async function connectionClient() {
+    if (!client) {
+        client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    }
+    await client.connect();
+
+}
+
 export const getAllUsers = (req, res) => {
     res.send("all users");
 };
 
-export const signup = (req, res) => {
-    res.send("sign up");
+export const signup = async (req, res) => {
+    const { username, password, email } = req.body;
+    try {
+        await connectionClient();
+        const db = client.db("githubclone")
+        const userCollection = db.collection("users");
+
+        const user = await userCollection.findOne({ username });
+
+        if (user) {
+            return res.status(400).json({ message: "user already exists" })
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = {
+            username,
+            password: hashedPassword,
+            email,
+            repositories: [],
+            followedUsers: [],
+            starRepos: []
+        }
+
+        const result = await userCollection.insertOne(newUser);
+        const token = jwt.sign({ id: result.insertId }, process.env.JWT_SECRET_KEY, { expiresIn: "1h" })
+        res.send({ token });
+    } catch (error) {
+        console.log("err during signup", error);
+
+    }
 };
 
 export const login = (req, res) => {
